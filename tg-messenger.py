68a8c406dd58991f1d95101a4599b578ebaf3004
•ᴘɪɴᴏˣᴰ <pinoq@protonmail.com>
2019-01-16 00:00:35 +0330

    new update!
    answering option added!

--- tg-messenger.py
+++ tg-messenger.py
@@ -3,40 +3,49 @@ import requests
 from time import sleep
 
 session = requests.session()
-session.proxies = { 
-                  }
-### set the proxy ( optional )
-API_TOKEN = ''
-### set your token
+session.proxies = dict() ### proxy ( optional )
+
+API_TOKEN = str() ### token of the bot
+AdminID = str() ### numberical ID
 UID = int()
 while True:
-    UpLis = open('file','r')
-    ### open the .txt file
+    UpLis = open('file','r') ### adress of the .txt file ( put it instead of file )
     UID = int(UpLis.readline())
     COMMAND = 'getupdates?offset=%s' % str(UID+1)
     RESPONSE = session.get('https://api.telegram.org/bot%s/%s'%(API_TOKEN,COMMAND), proxies = session.proxies)
     if len(RESPONSE.json()['result']) != 0:
-        COMMAND = 'sendmessage'
-        for i in range(len(RESPONSE.json()['result'])):
-            session.post('https://api.telegram.org/bot%s/%s?chat_id=%s&text=Sent!+:)&reply_to_message_id=%s'%(API_TOKEN, COMMAND,
-            (RESPONSE.json()['result'][i]['message']['chat']['id']),
-            (RESPONSE.json()['result'][i]['message']['message_id'])
-            ),
-            proxies = session.proxies,
-            )
-            COMMAND = 'forwardMessage'
-            session.post('https://api.telegram.org/bot%s/%s?chat_id=
-            ### erase this whole line and write your nimberical ID! ( get your numberical id using 'userinfobot' )
-            &from_chat_id=%s&message_id=%s'%(API_TOKEN,COMMAND,
-            (RESPONSE.json()['result'][i]['message']['chat']['id']),
-            (RESPONSE.json()['result'][i]['message']['message_id'])
-            ),
-            proxies = session.proxies,
-            )
-            UID = int(RESPONSE.json()['result'][i]['update_id'])
+        for i in RESPONSE.json()['result']:
+            if i['message']['from']['id'] == int(AdminID):
+                try:
+                    COMMAND = 'forwardMessage'
+                    session.post('https://api.telegram.org/bot%s/%s?chat_id=%s&from_chat_id=%s&message_id=%s'%(API_TOKEN,COMMAND,
+                    (i['message']['reply_to_message']['forward_from']['id']),
+                    (i['message']['chat']['id']),
+                    (i['message']['message_id'])
+                    ),
+                    proxies = session.proxies,
+                    )
+                except:
+                    pass
+            else:
+                COMMAND = 'sendmessage'
+                session.post('https://api.telegram.org/bot%s/%s?chat_id=%s&text=Sent!+:)&reply_to_message_id=%s'%(API_TOKEN, COMMAND,
+                (i['message']['chat']['id']),
+                (i['message']['message_id'])
+                ),
+                proxies = session.proxies,
+                )
+                COMMAND = 'forwardMessage'
+                session.post('https://api.telegram.org/bot%s/%s?chat_id=%s&from_chat_id=%s&message_id=%s'%(API_TOKEN,COMMAND,
+                AdminID,
+                (i['message']['chat']['id']),
+                (i['message']['message_id'])
+                ),
+                proxies = session.proxies,
+                )
+            UID = int(i['update_id'])
     UpLis.close()
-    UpLis = open('file','w')
-    ### open the file again !
+    UpLis = open('file','w') ### adress of the .txt file ( put it instead of file )
     UpLis.writelines(str(UID))
     UpLis.close()
     sleep(2)
